81f76438534c0dc4b2a7c53a5b122e8253da3b39
Touhidul Shawan <touhidulshawan@gmail.com>
2021-09-16 11:31:30 +0600

    add some comments

--- condition.sh
+++ condition.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
+# Author: Touhidul Shawan
+# Date Created: 16-09-2021
+# Last Modified: 16-09-2021
+
+# Description
+# Print Yep if count is equal to 5
+# Dummy Login System
+# Dummy Voting System
+# Getting Food System
+
+# Usage
+# Enter a username
+# Enter a password
+# Enter your age
+# Choose a fruit from option
+
 count=5
 
 if [ $count -eq 5 ]; then
@@ -11,13 +27,12 @@ pass="linux"
 
 read -p "Enter your username: " username
 read -sp "Enter your password": password
-echo 
+echo
 if [[ $password = $pass ]]; then
     echo "Successfully login! Welcome $username"
 else
     echo "Password not match"
 fi
-
 
 # conditional AND [ && ] operator
 # validation for vote
@@ -26,7 +41,7 @@ echo -e "Enter your age: \c"
 read age
 
 if [[ $age -gt 17 && $age -gt 24 ]]; then
-   echo "Can vote and do election" 
+    echo "Can vote and do election"
 elif [[ $age -lt 25 && $age -gt 17 ]]; then
     echo "Only can vote"
 else
@@ -39,7 +54,7 @@ echo -e "Choose a Fruit: \c"
 read fruit_name
 
 if [[ $fruit_name == "banana" || $fruit_name == "mango" ]]; then
-   echo "You will get free milk" 
+    echo "You will get free milk"
 elif [[ $fruit_name == "pineapple" ]]; then
     echo "You will get a pineapple juice"
 else
