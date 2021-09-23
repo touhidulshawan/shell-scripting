e284632fc387501c2250f05e398fd45239333539
Touhidul Shawan <touhidulshawan@gmail.com>
2021-09-23 15:19:52 +0600

    add promt to user input in case

--- case.sh
+++ case.sh
@@ -1,16 +1,23 @@
 #! /bin/bash
 
-vehicle=$1
+read -t 5 -p "Enter which type of vehicle you want to rent(car, van, bicycle,
+truck) > " vehicle
+echo ""
 
-case $vehicle in
-    "car" )
-        echo "Rent of $vehicle is 100 dollor";;
-    "van" )
-        echo "Rent of $vehicle is 80 dollor";;
-    "bicycle" )
-        echo "Rent of $vehicle is 5 dollor";;
-    "truck" )
-        echo "Rent of $vehicle is 150 dollor";;
-    * )
-        echo "Unknown Vehicle";;
+case "$vehicle" in
+"car")
+    echo "Rent of $vehicle is 100 dollor"
+    ;;
+"van")
+    echo "Rent of $vehicle is 80 dollor"
+    ;;
+"bicycle")
+    echo "Rent of $vehicle is 5 dollor"
+    ;;
+"truck")
+    echo "Rent of $vehicle is 150 dollor"
+    ;;
+*)
+    echo "Unknown Vehicle"
+    ;;
 esac
